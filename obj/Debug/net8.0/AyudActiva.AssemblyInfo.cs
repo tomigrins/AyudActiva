f2457ce6310b0b4af649452b4d07a58dd573bf0a
CANELA LEDEZMA <49193970@ort>
2025-11-20 14:46:44 -0300

    Index cambios en reseñas

--- obj/Debug/net8.0/AyudActiva.AssemblyInfo.cs
+++ obj/Debug/net8.0/AyudActiva.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AyudActiva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d577c3ae45521ffbe018db6ea563674d49e22bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2bd229b54183e54b4e18d42212b16b4d4bf306a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AyudActiva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AyudActiva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
